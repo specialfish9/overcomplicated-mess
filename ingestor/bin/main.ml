d0a9bdb38140b4839ac2a7697cc0519c5931c908
Mattia Girolimetto <mattia.girolimetto@studio.unibo.it>
2024-01-07 17:22:45 +0100

    ci

--- ingestor/bin/main.ml
+++ ingestor/bin/main.ml
@@ -1,8 +1,9 @@
 open Appconf.Conf
 
 let init = 
+  print_endline (Printf.sprintf "\nInflux endpoint: %s" influxdb_url);
   let routes = Http_handler.post "/push" Routes.push_metric in
-  print_endline (Printf.sprintf "\nListening on port %d..." port);
+  print_endline (Printf.sprintf "Listening on port %d..." port);
   Http_handler.new_server port routes 
   
 let () = ignore (Lwt_main.run (init))
